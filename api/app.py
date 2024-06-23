d6cec697dbe69a2a1a89ac172b87d19dbfd0158a
Jack Luo <jack145945@gmail.com>
2024-06-23 00:37:15 -0700

    text.py iteration 1
    Co-authored-by: Ivan Yeung <theivanyeung@users.noreply.github.com>
    Co-authored-by: bhanumayankjha <bhanumayankjha@users.noreply.github.com>

--- api/app.py
+++ api/app.py
@@ -1,9 +1,24 @@
+
+# Imports
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import traceback
+import requests
 
+import asyncio
+from hume import HumeVoiceClient, MicrophoneInterface
+
+# ENV
+
+HUME_API_KEY = "3LpGtjH4qrFPqAq6ySRYuAA6IV8zkSpxtiYHs1nqpwvK7FWY"
+HUME_SECRET_KEY = "CkpfSUeJtApvpbmrFnHTqpGgSKf9bl3jndfihMYVakHkBShv1wMlLmcwsd7yalGG"
+
+# Config
 app = Flask(__name__)
 CORS(app)
+
+client = HumeVoiceClient(HUME_API_KEY)
 
 # Flask 
 
@@ -14,6 +29,19 @@ def handle_internal_server_error(error):
 @app.route("/")
 def home():
     return "If humanity successfully builds AGI, how the hell are we gonna control it? In the meantime, I'm building this with the end goal of eventually helping me do shit while I'm operating my digital world. Starting out with an AI specializing in distraction management."
+
+# API
+
+@app.route("/engine/voice", methods=["POST"])
+def voice_classifier():
+
+    voice = request.files['voice']
+
+    pass
+
+def analyze_hume_voice():
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
